6e1a81a645b455c77ac1d8dbb229e981c8592bda
khoanguyen59 <phuongkhoa95ntp@gmail.com>
2020-08-26 13:18:55 +0700

    Add Service

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post,Put, Delete, Body, Param } from '@nestjs/common';
 import { UserEntity } from '../user.entity';
 import { UserService } from './user.service';
 
@@ -10,4 +10,21 @@ export class UserController {
     index(): Promise<UserEntity[]> {
       return this.usersService.findAll();
     }    
+
+    @Post('create')
+    async create(@Body() userData: UserEntity): Promise<any> {
+      return this.usersService.create(userData);
+    }  
+
+    @Put(':id/update')
+    async update(@Param('id') id, @Body() userData: UserEntity): Promise<any> {
+        userData.id = id;
+        console.log('Update #' + userData.id)
+        return this.usersService.update(userData);
+    }  
+
+    @Delete(':id/delete')
+    async delete(@Param('id') id): Promise<any> {
+      return this.usersService.delete(id);
+    }  
 }
